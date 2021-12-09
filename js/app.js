d79554af1d3bf2d6ab6dc1a53ae5faf4ffa91041
foyjunnesa <foyjunnesamayna@gmail.com>
2021-12-09 21:33:18 +0600

    merry christmas

--- js/app.js
+++ js/app.js
@@ -17,10 +17,10 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
-      <footer>${products.rating}</footer>
+      <footer>Rating: ${product.rating.rate}</footer>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-white">add to cart</button>
+      <button id="details-btn" class="btn btn-success">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
